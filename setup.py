23da934c4a9eaff40dcc772c4ae906e8074da209
Elmar Athmer <elmar@nixus-minimax.de>
2011-02-17 22:19:36 +0100

    - added command packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name="django-tweet-saved",
       author_email="elmar@nixus-minimax.de",
       url="https://github.com/zauberpony/django-tweet-saved",
       version='0.8',
-      packages=['tweet', 'tweet.migrations'],
+      packages=['tweet', 'tweet.migrations', 'tweet.management', 'tweet.management.commands'],
       install_requires=[
               'twitter',
               'python-googl'
